4e4cb3d041cdb942e85c7cec29e27e743e837457
Alejo Gaitán <thy.gaitan@gmail.com>
2026-05-15 16:01:26 -0300

    fixes

--- EcosDelHielo/Assets/Scripts/Core/TriggerNotifier.cs
+++ EcosDelHielo/Assets/Scripts/Core/TriggerNotifier.cs
@@ -6,6 +6,8 @@ namespace Core
     public class TriggerNotifier : MonoBehaviour
     {
         public Action<Collider2D> OnEntered;
+        public Action<Collider2D> OnExited;
         private void OnTriggerEnter2D(Collider2D other) => OnEntered?.Invoke(other);
+        private void OnTriggerExit2D(Collider2D other)  => OnExited?.Invoke(other);
     }
 }
